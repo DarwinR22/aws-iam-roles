59d152162260c7a2b4455d56b8734b0ee4ef1c66
darwin-lopez_claroca <darwin.lopez@claro.com.gt>
2025-10-09 14:49:39 -0600

    MOD: cambiar tags a mayusculas y limpiar modulos obsoletos - Pais: rg -> RG - Ambiente: dev -> DEV - Eliminados 16 directorios obsoletos de modules/policies - Solo quedan 9 modulos activos (7 ABAC + 2 legacy) - Regenerados todos los modulos Terraform

--- generated/modules/policies/mci_github_deployment_terraform_state/main.tf
+++ generated/modules/policies/mci_github_deployment_terraform_state/main.tf
@@ -2,7 +2,7 @@
 data "aws_caller_identity" "current" {}
 
 # github-deployment-terraform-state Policy Module - Following ABAC Pattern
-# Converted from definitions/policies/deployment/github-deployment-terraform-state.yaml
+# Converted from definitions/policies/deployment\github-deployment-terraform-state.yaml
 resource "aws_iam_policy" "main" {
   name        = "github-deployment-terraform-state"
   path        = "/"
@@ -12,16 +12,16 @@ resource "aws_iam_policy" "main" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "TerraformS3StateAccess"
-        Effect   = "Allow"
-        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket", "s3:GetBucketVersioning", "s3:GetBucketLocation", "s3:ListBucketVersions"]
-        Resource = ["arn:aws:s3:::*-tfstate-*", "arn:aws:s3:::*-tfstate-*/*"]
-        }, {
-        Sid      = "TerraformDynamoDBLockAccess"
-        Effect   = "Allow"
-        Action   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem", "dynamodb:DescribeTable", "dynamodb:DescribeContinuousBackups"]
-        Resource = ["arn:aws:dynamodb:*:*:table/*-terraform-lock*", "arn:aws:dynamodb:*:*:table/*-tfstate-lock*"]
-    }]
+Sid    = "TerraformS3StateAccess"
+Effect = "Allow"
+        Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket", "s3:GetBucketVersioning", "s3:GetBucketLocation", "s3:ListBucketVersions"]
+        Resource = [          "arn:aws:s3:::*-tfstate-*",          "arn:aws:s3:::*-tfstate-*/*"        ]
+      },      {
+Sid    = "TerraformDynamoDBLockAccess"
+Effect = "Allow"
+        Action = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem", "dynamodb:DescribeTable", "dynamodb:DescribeContinuousBackups"]
+        Resource = [          "arn:aws:dynamodb:*:*:table/*-terraform-lock*",          "arn:aws:dynamodb:*:*:table/*-tfstate-lock*"        ]
+      }    ]
   })
 
   tags = merge(var.common_tags, {
