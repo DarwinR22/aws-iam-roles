63a4ca40acff8217cf7394d67437fa2633716d67
darwin-lopez_claroca <darwin.lopez@claro.com.gt>
2025-10-09 14:31:33 -0600

    MOD: aplicar correctamente fix de nombre y path en politicas - Eliminado prefijo mci- hardcoded (mci-cloudwatch en vez de mci-mci-cloudwatch) - Path cambiado a / (en vez de /policies/) - Regenerados modulos con configuracion correcta

--- generated/modules/policies/mci_eventbridge_deploymentabac/main.tf
+++ generated/modules/policies/mci_eventbridge_deploymentabac/main.tf
@@ -2,40 +2,40 @@
 data "aws_caller_identity" "current" {}
 
 # MCI-EventBridge-DeploymentABAC Policy Module - Following ABAC Pattern
-# Converted from definitions/policies/deployment/MCI-EventBridge-DeploymentABAC.yaml
+# Converted from definitions/policies/deployment\MCI-EventBridge-DeploymentABAC.yaml
 resource "aws_iam_policy" "main" {
-  name        = "mci-mci-eventbridge-deploymentabac"
-  path        = "/policies/"
+  name        = "mci-eventbridge-deploymentabac"
+  path        = "/"
   description = var.policy_description
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "EventBridgeFullManagement"
-        Effect   = "Allow"
-        Action   = ["events:PutRule", "events:DeleteRule", "events:DescribeRule", "events:EnableRule", "events:DisableRule", "events:ListRules", "events:PutTargets", "events:RemoveTargets", "events:ListTargetsByRule", "events:PutEvents", "events:TagResource", "events:UntagResource", "events:ListTagsForResource"]
-        Resource = ["arn:aws:events:*:*:rule/*"]
+Sid    = "EventBridgeFullManagement"
+Effect = "Allow"
+        Action = ["events:PutRule", "events:DeleteRule", "events:DescribeRule", "events:EnableRule", "events:DisableRule", "events:ListRules", "events:PutTargets", "events:RemoveTargets", "events:ListTargetsByRule", "events:PutEvents", "events:TagResource", "events:UntagResource", "events:ListTagsForResource"]
+        Resource = [          "arn:aws:events:*:*:rule/*"        ]
         Condition = {
-        StringEquals = { "aws:RequestTag/gerencia" = ["$${aws:PrincipalTag/gerencia}"] } }
-        }, {
-        Sid      = "EventBridgeSchedulerManagement"
-        Effect   = "Allow"
-        Action   = ["scheduler:CreateSchedule", "scheduler:DeleteSchedule", "scheduler:GetSchedule", "scheduler:UpdateSchedule", "scheduler:ListSchedules", "scheduler:CreateScheduleGroup", "scheduler:DeleteScheduleGroup", "scheduler:GetScheduleGroup", "scheduler:ListScheduleGroups", "scheduler:TagResource", "scheduler:UntagResource"]
-        Resource = ["arn:aws:scheduler:*:*:schedule/*", "arn:aws:scheduler:*:*:schedule-group/*"]
-        }, {
-        Sid      = "StepFunctionsFullManagement"
-        Effect   = "Allow"
-        Action   = ["states:CreateStateMachine", "states:DeleteStateMachine", "states:UpdateStateMachine", "states:DescribeStateMachine", "states:ListStateMachines", "states:StartExecution", "states:StopExecution", "states:DescribeExecution", "states:GetExecutionHistory", "states:ListExecutions", "states:TagResource", "states:UntagResource", "states:ListTagsForResource"]
-        Resource = ["arn:aws:states:*:*:stateMachine:*", "arn:aws:states:*:*:execution:*:*"]
+          StringEquals = {            "aws:RequestTag/gerencia" = ["$${aws:PrincipalTag/gerencia}"]          }        }
+      },      {
+Sid    = "EventBridgeSchedulerManagement"
+Effect = "Allow"
+        Action = ["scheduler:CreateSchedule", "scheduler:DeleteSchedule", "scheduler:GetSchedule", "scheduler:UpdateSchedule", "scheduler:ListSchedules", "scheduler:CreateScheduleGroup", "scheduler:DeleteScheduleGroup", "scheduler:GetScheduleGroup", "scheduler:ListScheduleGroups", "scheduler:TagResource", "scheduler:UntagResource"]
+        Resource = [          "arn:aws:scheduler:*:*:schedule/*",          "arn:aws:scheduler:*:*:schedule-group/*"        ]
+      },      {
+Sid    = "StepFunctionsFullManagement"
+Effect = "Allow"
+        Action = ["states:CreateStateMachine", "states:DeleteStateMachine", "states:UpdateStateMachine", "states:DescribeStateMachine", "states:ListStateMachines", "states:StartExecution", "states:StopExecution", "states:DescribeExecution", "states:GetExecutionHistory", "states:ListExecutions", "states:TagResource", "states:UntagResource", "states:ListTagsForResource"]
+        Resource = [          "arn:aws:states:*:*:stateMachine:*",          "arn:aws:states:*:*:execution:*:*"        ]
         Condition = {
-        StringEquals = { "aws:RequestTag/gerencia" = ["$${aws:PrincipalTag/gerencia}"] } }
-        }, {
-        Sid      = "StepFunctionsActivityManagement"
-        Effect   = "Allow"
-        Action   = ["states:CreateActivity", "states:DeleteActivity", "states:DescribeActivity", "states:GetActivityTask", "states:ListActivities", "states:SendTaskSuccess", "states:SendTaskFailure", "states:SendTaskHeartbeat"]
-        Resource = ["arn:aws:states:*:*:activity:*"]
-    }]
+          StringEquals = {            "aws:RequestTag/gerencia" = ["$${aws:PrincipalTag/gerencia}"]          }        }
+      },      {
+Sid    = "StepFunctionsActivityManagement"
+Effect = "Allow"
+        Action = ["states:CreateActivity", "states:DeleteActivity", "states:DescribeActivity", "states:GetActivityTask", "states:ListActivities", "states:SendTaskSuccess", "states:SendTaskFailure", "states:SendTaskHeartbeat"]
+        Resource = [          "arn:aws:states:*:*:activity:*"        ]
+      }    ]
   })
 
   tags = merge(var.common_tags, {
--- generated/modules/policies/mci_terraform_statemanagement/main.tf
+++ generated/modules/policies/mci_terraform_statemanagement/main.tf
@@ -2,26 +2,26 @@
 data "aws_caller_identity" "current" {}
 
 # MCI-Terraform-StateManagement Policy Module - Following ABAC Pattern
-# Converted from definitions/policies/deployment/MCI-Terraform-StateManagement.yaml
+# Converted from definitions/policies/deployment\MCI-Terraform-StateManagement.yaml
 resource "aws_iam_policy" "main" {
-  name        = "mci-mci-terraform-statemanagement"
-  path        = "/policies/"
+  name        = "mci-terraform-statemanagement"
+  path        = "/"
   description = var.policy_description
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "TerraformS3StateAccess"
-        Effect   = "Allow"
-        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket", "s3:GetBucketVersioning", "s3:GetBucketLocation", "s3:ListBucketVersions"]
-        Resource = ["arn:aws:s3:::*-tfstate-*", "arn:aws:s3:::*-tfstate-*/*"]
-        }, {
-        Sid      = "TerraformDynamoDBLockAccess"
-        Effect   = "Allow"
-        Action   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem", "dynamodb:DescribeTable", "dynamodb:DescribeContinuousBackups"]
-        Resource = ["arn:aws:dynamodb:*:*:table/*-terraform-lock*", "arn:aws:dynamodb:*:*:table/*-tfstate-lock*"]
-    }]
+Sid    = "TerraformS3StateAccess"
+Effect = "Allow"
+        Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket", "s3:GetBucketVersioning", "s3:GetBucketLocation", "s3:ListBucketVersions"]
+        Resource = [          "arn:aws:s3:::*-tfstate-*",          "arn:aws:s3:::*-tfstate-*/*"        ]
+      },      {
+Sid    = "TerraformDynamoDBLockAccess"
+Effect = "Allow"
+        Action = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem", "dynamodb:DescribeTable", "dynamodb:DescribeContinuousBackups"]
+        Resource = [          "arn:aws:dynamodb:*:*:table/*-terraform-lock*",          "arn:aws:dynamodb:*:*:table/*-tfstate-lock*"        ]
+      }    ]
   })
 
   tags = merge(var.common_tags, {
