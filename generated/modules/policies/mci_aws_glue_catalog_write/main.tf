d67e583a19ef39bf90d160bc4475a0cb2d8993b7
darwin-lopez_claroca <darwin.lopez@claro.com.gt>
2025-10-17 10:34:20 -0600

    MOD: Remove ABAC Gerencia restriction from EventBridge/StepFunctions deployment policy - Enable multi-gerencia event orchestration

--- generated/modules/policies/mci_aws_glue_catalog_write/main.tf
+++ generated/modules/policies/mci_aws_glue_catalog_write/main.tf
@@ -2,7 +2,7 @@
 data "aws_caller_identity" "current" {}
 
 # mci-aws-glue-catalog-write Policy Module - Following ABAC Pattern
-# Converted from definitions/policies/execution/mci-aws-glue-catalog-write.yaml
+# Converted from definitions/policies/execution\mci-aws-glue-catalog-write.yaml
 resource "aws_iam_policy" "main" {
   name        = "mci-aws-glue-catalog-write"
   path        = "/"
@@ -12,13 +12,13 @@ resource "aws_iam_policy" "main" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "WriteGlueCatalogWithABAC"
-        Effect   = "Allow"
-        Action   = ["glue:CreateDatabase", "glue:UpdateDatabase", "glue:DeleteDatabase", "glue:CreateTable", "glue:UpdateTable", "glue:DeleteTable", "glue:BatchCreatePartition", "glue:CreatePartition", "glue:UpdatePartition", "glue:DeletePartition", "glue:BatchDeletePartition", "glue:BatchUpdatePartition"]
-        Resource = ["arn:aws:glue:*:*:catalog", "arn:aws:glue:*:*:database/*", "arn:aws:glue:*:*:table/*/*"]
+Sid    = "WriteGlueCatalogWithABAC"
+Effect = "Allow"
+        Action = ["glue:CreateDatabase", "glue:UpdateDatabase", "glue:DeleteDatabase", "glue:CreateTable", "glue:UpdateTable", "glue:DeleteTable", "glue:BatchCreatePartition", "glue:CreatePartition", "glue:UpdatePartition", "glue:DeletePartition", "glue:BatchDeletePartition", "glue:BatchUpdatePartition"]
+        Resource = [          "arn:aws:glue:*:*:catalog",          "arn:aws:glue:*:*:database/*",          "arn:aws:glue:*:*:table/*/*"        ]
         Condition = {
-        StringEquals = { "aws:PrincipalTag/Cuenta" = ["$${aws:ResourceTag/Cuenta}"] } }
-    }]
+          StringEquals = {            "aws:PrincipalTag/Cuenta" = ["$${aws:ResourceTag/Cuenta}"]          }        }
+      }    ]
   })
 
   tags = merge(var.common_tags, {
