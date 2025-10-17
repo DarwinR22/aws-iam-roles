e68901a9f7aaa516f5d0de3e94a08504082f9fa9
darwin-lopez_claroca <darwin.lopez@claro.com.gt>
2025-10-17 10:56:09 -0600

    ADD: SNS GetSubscriptionAttributes and SetSubscriptionAttributes permissions to CloudWatch deployment policy - Fix Terraform state refresh errors

--- generated/modules/policies/mci_aws_glue_catalog_read/main.tf
+++ generated/modules/policies/mci_aws_glue_catalog_read/main.tf
@@ -2,7 +2,7 @@
 data "aws_caller_identity" "current" {}
 
 # mci-aws-glue-catalog-read Policy Module - Following ABAC Pattern
-# Converted from definitions/policies/execution/mci-aws-glue-catalog-read.yaml
+# Converted from definitions/policies/execution\mci-aws-glue-catalog-read.yaml
 resource "aws_iam_policy" "main" {
   name        = "mci-aws-glue-catalog-read"
   path        = "/"
@@ -12,13 +12,13 @@ resource "aws_iam_policy" "main" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "ReadGlueCatalogWithABAC"
-        Effect   = "Allow"
-        Action   = ["glue:GetDatabase", "glue:GetDatabases", "glue:GetTable", "glue:GetTables", "glue:GetPartition", "glue:GetPartitions", "glue:BatchGetPartition", "glue:GetTableVersion", "glue:GetTableVersions", "glue:SearchTables"]
-        Resource = ["arn:aws:glue:*:*:catalog", "arn:aws:glue:*:*:database/*", "arn:aws:glue:*:*:table/*/*"]
+Sid    = "ReadGlueCatalogWithABAC"
+Effect = "Allow"
+        Action = ["glue:GetDatabase", "glue:GetDatabases", "glue:GetTable", "glue:GetTables", "glue:GetPartition", "glue:GetPartitions", "glue:BatchGetPartition", "glue:GetTableVersion", "glue:GetTableVersions", "glue:SearchTables"]
+        Resource = [          "arn:aws:glue:*:*:catalog",          "arn:aws:glue:*:*:database/*",          "arn:aws:glue:*:*:table/*/*"        ]
         Condition = {
-        StringEquals = { "aws:PrincipalTag/Cuenta" = ["$${aws:ResourceTag/Cuenta}"] } }
-    }]
+          StringEquals = {            "aws:PrincipalTag/Cuenta" = ["$${aws:ResourceTag/Cuenta}"]          }        }
+      }    ]
   })
 
   tags = merge(var.common_tags, {
